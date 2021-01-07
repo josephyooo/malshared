627f6a2b3d692784a250bef26d96e70acf6b90c5
joseph <31323641+josephyooo@users.noreply.github.com>
2021-01-07 00:06:05 -0500

    put it inna class

--- main.py
+++ main.py
@@ -1,30 +1,107 @@
 from requests import get
 
-def getshared(token, parameters, users):
-    headers = {
-        'Authorization': 'Bearer '+token
-    }
-    parameters='&'.join(['='.join(i) for i in parameters.items()])
-    shared={}
-    errors={}
-    for user in users:
+class Shared():
+    def __init__(self, token, users, parameters):
+        self.token = token
+        self.users = users
+        self.parameters = parameters
+        self.data=self.getshared(self.users)
+        self.table=self.gentable(self.data, self.users)
+        # self.formatted=self.filter(self.table)
+        # self.formatted=self.format_table(self.formatted)
+        self.formatted=self.format_table(self.table)
+
+    def getanime(self, user, token):
+        headers = {
+            'Authorization': 'Bearer '+token
+        }
+        parameters='&'.join(['='.join(i) for i in self.parameters.items()])
+        
         r=get(f"https://api.myanimelist.net/v2/users/{user}/animelist?{parameters}", headers=headers)
+        return r
 
-        if r.status_code==200:
-            r=r.json()
-            for i in r['data']:
-                if i['node']['title'] not in shared:
-                    shared[i['node']['title']]={
-                        'data': {},
-                        'id': i['node']['id'],
-                        'main_picture': i['node']['main_picture']
-                    }
+    def getshared(self, users):
+        shared={}
+        errors={}
+        for user in users:
+            r=self.getanime(user, self.token)
+            if r.status_code==200:
+                r=r.json()
+                for i in r['data']:
+                    if i['node']['title'] not in shared:
+                        shared[i['node']['title']]={
+                            'data': {},
+                            'id': i['node']['id'],
+                            'main_picture': i['node']['main_picture']
+                        }
 
-                shared[i['node']['title']]['data'][user]=i['list_status']
-        else:
-            errors[user]=[r.status_code,r.json()]
-    
-    return shared, errors
+                    shared[i['node']['title']]['data'][user]=i['list_status']
+            else:
+                errors[user]=[r.status_code,r.json()]
+        
+        return shared, errors
+
+
+    # quick helper
+    def _avg(self, x):
+        try:
+            return sum(x)/len(x)
+        except ZeroDivisionError:
+            return ''
+
+    def gentable(self, data, users):
+        table=[['Title']]
+        users=[i for i in users if i not in data[1]]
+        table[0]+=users
+        table[0].append('average score')
+        # generate table template
+        for i in data[0]:
+            row=[i]+['']*len(users)
+            i=data[0][i]
+            # generate row template
+            for p in i['data']:
+                if i['data'][p]['score']==0:
+                    row[users.index(p)+1]='ptw'
+                else:
+                    row[users.index(p)+1]=i['data'][p]['score']
+            row.append(self._avg([i for i in row if isinstance(i, int)]))
+            
+            table.append(row)
+
+        return table
+
+    def format_table(self, table):
+        # gets # of columns, adds lengths of each row/column, adds max to lengths list
+        # to figure out how much whitespace needs to be added
+        # (kinda really relies on monospace font)
+
+        # i think i could do in one line but idrk
+        lengths=[]
+        for ci in range(len(table[0])):
+            x=[]
+            for r in table:
+                x.append(len(str(r[ci])))
+            lengths.append(max(x))
+
+            for r in table:
+                r[ci]=f'{str(r[ci]):^{lengths[ci]}}'
+
+        table=['|'.join([str(a) for a in i]) for i in table]
+        return table
+
+    def filter(self, table, minimum):
+        # filter hack for filtering lists with only ptw
+        # for r in table:
+        #     if not r[-1]:
+        #         table.remove(r)
+
+        
+        # once i start adding variable metrics, this wont be okay
+        for r in table[1:]:
+            if sum([1 for i in r[1:-1] if isinstance(i, int)])<minimum:
+                table.remove(r)
+        
+        return table
 
 def gettoken():
         from sys import exit
@@ -45,53 +122,6 @@ def gettoken():
         print("paste above token into config.py and restart")
         exit()
 
-# quick helper
-def avg(x):
-    try:
-        return sum(x)/len(x)
-    except ZeroDivisionError:
-        return 'n/a'
-
-def gentable(shared, users):
-    table=[['Title']]
-    users=[i for i in users if i not in shared[1]]
-    table[0]+=users
-    table[0].append('average score')
-    # generate table template
-    for i in shared[0]:
-        row=[i]+['']*len(users)
-        i=shared[0][i]
-        # generate row template
-        for p in i['data']:
-            if i['data'][p]['score']==0:
-                row[users.index(p)+1]='ptw'
-            else:
-                row[users.index(p)+1]=i['data'][p]['score']
-        row.append(avg([i for i in row if isinstance(i, int)]))
-        
-        table.append(row)
-
-    return table
-
-def format_table(table):
-    # gets # of columns, adds lengths of each row/column, adds max to lengths list
-    # to figure out how much whitespace needs to be added
-    # (kinda really relies on monospace font)
-
-    # i think i could do in one line but idrk
-    lengths=[]
-    for ci in range(len(table[0])):
-        x=[]
-        for r in table:
-            x.append(len(str(r[ci])))
-        lengths.append(max(x))
-
-        for r in table:
-            r[ci]=f'{str(r[ci]):^{lengths[ci]}}'
-
-    table=['|'.join([str(a) for a in i]) for i in table]
-    return table
-
 if __name__ == "__main__":
     try:
         from config import token
@@ -101,17 +131,21 @@ if __name__ == "__main__":
     parameters = {
         'fields': 'list_status',
         'sort': 'anime_title',
-        'limit': '10'
+        'limit': '3'
     }
-    
     users=[]
 
-    shared=getshared(token, parameters, users)
-    table=gentable(shared, users)
-    table=format_table(table)
+    # responses=getanime(token, parameters, users)
+    # print([r.status_code for r in responses])
+    # shared=getshared(responses)
+    # table=gentable(shared, users)
+    # # table=filter(table)
+    # table=format_table(table)
 
-    # for i in table: print(i)
+    shared=Shared(token, users, parameters)
+
     with open('output.txt', 'w+', encoding='utf-8') as output:
-        for i in table: output.write(str(i)+'\n')
+        for i in shared.formatted:
+            output.write(str(i)+'\n')
 
 print()
