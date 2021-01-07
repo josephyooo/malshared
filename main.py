1b13640616d872e7463ba43c193c2fd7be876415
joseph <31323641+josephyooo@users.noreply.github.com>
2021-01-06 21:32:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def gentable(shared, users):
         
         table.append(row)
 
-    # table=['|'.join([str([a for a in i])]) for i in table]
+    table=['|'.join([str([a for a in i])]) for i in table]
     return table
 
 def format_table(table):
